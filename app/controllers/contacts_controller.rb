a2d7dab1fe36db139e8184ff48e52797f54b893e
heathersherry <247373319@qq.com>
2012-08-08 20:23:20 +0800

    reupload

--- app/controllers/contacts_controller.rb
+++
@@ -1,8 +0,0 @@
-class ContactsController < ApplicationController
-  skip_before_filter :authorize
-
-  def index
-    @cart = current_cart
-  end
-
-end
--- app/controllers/searches_controller.rb
+++
@@ -1,8 +0,0 @@
-class SearchesController < ApplicationController
-  
-  def index
-    if params[:q]
-      @results = Product.search(params[:q])
-    end
-  end
-end
