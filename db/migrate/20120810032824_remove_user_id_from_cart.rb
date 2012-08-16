b10116ab2415831308067f57cc28822c01d112d9
heathersherry <247373319@qq.com>
2012-08-16 10:38:41 +0800

    delete migrate files

--- db/migrate/20120810032824_remove_user_id_from_cart.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromCart < ActiveRecord::Migration
-  def self.up
-    remove_column :carts, :user_id
-  end
-
-  def self.down
-    add_column :carts, :user_id, :string
-  end
-end
